44d19a24c9978e244a0ecfce937a8ebdc2fbdb9e
ahmed raza <ahamedraza2233@gmail.com>
2025-11-01 14:52:40 +0530

    slkfj

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,37 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// import { Html, Head, Main, NextScript } from 'next/document'
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
+
+
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ✅ Add your favicon here */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        {/* (Optional) For better browser support */}
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        {/* <link rel="apple-touch-icon" href="/favicon.png" /> */}
+        {/* <meta name="theme-color" content="#ffffff" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
